f837ca76fa0d22e8a0363c0aaf69ebc1c16dd5ff
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-08 14:22:05 -0700

    fix: show debug output

--- scripts/lifecycle.js
+++ scripts/lifecycle.js
@@ -1,10 +1,13 @@
-function run(pluginConfig, options) {
+function run({script}, options) {
   const path = require('path')
   const pjson = require(path.join(process.cwd(), 'package.json'))
   const exec = require('./exec')
   process.env.OCLIF_NEXT_VERSION = options.nextRelease.version
-  const cmd = pjson.scripts[pluginConfig.script]
-  if (cmd) exec(cmd)
+  const cmd = pjson.scripts[script]
+  if (cmd) {
+    console.log(`running ${script} script:\n$ ${cmd}`)
+    exec(cmd)
+  }
 }
 
 exports.prepare = run
--- scripts/update_version.js
+++ scripts/update_version.js
@@ -2,6 +2,8 @@ exports.prepare = (_, options) => {
   const fs = require('fs-extra')
   const path = require('path')
   const pjson = require(path.join(process.cwd(), 'package.json'))
-  pjson.version = options.nextRelease.version
+  const {version} = options.nextRelease
+  console.log(`updating package.json version from ${pjson.version} to ${version}`)
+  pjson.version = version
   fs.outputJSONSync('package.json', pjson, {spaces: 2})
 }
