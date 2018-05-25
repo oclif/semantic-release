2fad7e9b638464bd0c1de02856a6e06639d2e198
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-05-25 12:26:35 -0700

    fix: default pjson.scripts

--- scripts/lifecycle.js
+++ scripts/lifecycle.js
@@ -5,6 +5,7 @@ function run({script}, options) {
   const pjson = require(path.join(process.cwd(), 'package.json'))
   const exec = require('./exec')
   process.env.OCLIF_NEXT_VERSION = options.nextRelease.version
+  pjson.scripts = pjson.scripts || {}
   const cmd = pjson.scripts[script]
   if (cmd) {
     log(`running ${script} script:\n$ ${cmd}`)
