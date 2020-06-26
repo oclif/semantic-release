1bee757fa6d9eed8dba000c21af9067917628399
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2020-06-25 17:16:10 -0700

    fix: use execa.command & node:12

--- scripts/exec.js
+++ scripts/exec.js
@@ -3,6 +3,6 @@
 const execa = require('execa')
 const npmRunPath = require('npm-run-path')
 
-module.exports = (cmd, opts = {}) => {
-  execa.sync(cmd, {env: npmRunPath.env(), stdio: 'inherit', ...opts})
+module.exports = (cmd, args, opts = {}) => {
+  execa.command(cmd, args, {env: npmRunPath.env(), stdio: 'inherit', ...opts})
 }
